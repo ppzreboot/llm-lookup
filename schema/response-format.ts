08d3d24400d02a87bb9c4741ba2cf8fe75474414
PPz <ppzreboot@qq.com>
2025-11-11 00:18:08 +0800

    feat: response format

---
+++ schema/response-format.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { z_schema__maybe_word } from './schema.ts'
+
+export
+function make_format() {
+    return {
+        type: 'json_schema',
+        json_schema: {
+            name: '"English word" recognition result',
+            strict: true,
+            schema: z.toJSONSchema(z_schema__maybe_word, {
+                target: 'draft-7',
+            }),
+        }
+    }
+}
+
+console.log(make_format())
